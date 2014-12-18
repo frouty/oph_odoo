4774574a9fca829682d53710b209b67cf8e3cc02
Laurent FRANCOIS <francois.oph@gmail.com>
2014-12-19 02:25:07 +1100

    objet slot et day template en place. Menuitem slot OK

--- oph/wizard/__init__.py
+++ oph/wizard/__init__.py
@@ -11,6 +11,7 @@ import oph_formula_prescription
 import oph_ivt_prescription
 import oph_account_voucher_deposit
 import oph_crm_meeting_state
+import oph_day_template
 #===============================================================================
 # import oph_etat_factory
 # import oph_diabetic_report
---
+++ oph/wizard/oph_day_template.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import time
+from openerp.osv import osv, fields, orm
+from openerp.tools.translate import _
+
+class oph_slot(orm.Model):
+    """
+    A simple slot
+    for one appointment
+    parameters: 
+    -start_time qui est l'heure de début du crénaux. Il faudra voir pour le formatage 08:00:00, 08:00, 08h,08h00,8h
+    -duration en float qui est la durée du crénaux.
+    """
+    _name='oph.slot'
+    _columns={
+              'start_time':fields.char('Start Time', size=8),
+              'duration':fields.float('Duration'),
+              'day_id':fields.many2one('oph.day.template','Day Template'),}
+    
+    
+class oph_day_template(orm.Model):
+    """
+    TODO
+    """
+    _name='oph.day.template'
+    
+    _columns={
+              'name':fields.char('Name', help='A simple name', size=32),
+              'slot_ids':fields.one2many('oph.slot','day_id','Slots'),
+              }
