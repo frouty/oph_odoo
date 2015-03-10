69b20382517bb3e7998f6ab8166e20e2adec4465
Laurent FRANCOIS <francois.oph@gmail.com>
2015-03-11 08:41:08 +1100

    update

--- oph/wizard/oph_account_voucher_deposit.py
+++ oph/wizard/oph_account_voucher_deposit.py
@@ -36,7 +36,6 @@ class oph_account_voucher_deposit(osv.osv_memory):
         if context is None:
             context = {}
         modele = 'bdxcheck'
-
         pool_obj = pooler.get_pool(cr.dbname)
         data_inv = pool_obj.get(context.get('active_model')).read(cr, uid, context['active_ids'], ['journal_id','process_status'], context = context)
         journal_obj = self.pool.get('account.journal')
@@ -47,11 +46,8 @@ class oph_account_voucher_deposit(osv.osv_memory):
                 raise osv.except_osv(_('Error!'), _('Cannot Deposit a %s! Please Cancel and Select again') % (res.get('name'),))
             if rec.get('process_status')=='done':
                 raise osv.except_osv(_('Error!'), _('Cannot Deposit an already %s! Please Cancel and Select again') % (res.get('name'),))
-            
-
 #         from pdb import set_trace
 #         set_trace()
-
         for record in data_inv:
             pool_obj.get(context.get('active_model')).write(cr, uid, record['id'], {'check_deposit':True,'process_status':'process'}, context = context)
         data = self.pool.get('account.voucher').read(cr, uid, context.get('active_ids')[0], context = context)
@@ -68,7 +64,36 @@ class oph_account_voucher_deposit(osv.osv_memory):
                 'context':context
                 }
 
+    def account_voucher_clean(self, cr, uid, ids, context = None):
+        """Set In Process record to Done"""
+        if context is None:
+            context = {}
+        modele = 'bdxcheck'
+        pool_obj = pooler.get_pool(cr.dbname)
+        data_inv = pool_obj.get(context.get('active_model')).read(cr, uid, context['active_ids'], ['journal_id','process_status'], context = context)
+        journal_obj = self.pool.get('account.journal')
+        for rec in data_inv:
+            print  "REC:%s" % (rec,)
+            res = journal_obj.read(cr, uid, rec.get('journal_id')[0], ['name'], context = context)
+            if res.get('name') != u'Chèques':
+                raise osv.except_osv(_('Error!'), _('Cannot Deposit a %s! Please Cancel and Select again') % (res.get('name'),))
+            if rec.get('process_status')!='process':
+                raise osv.except_osv(_('Error!'), _('Cannot clean %s payments! Please Cancel and Select again') % (rec.get('process_status'),))
+#         from pdb import set_trace
+#         set_trace()
+        for record in data_inv:
+            pool_obj.get(context.get('active_model')).write(cr, uid, record['id'], {'check_deposit':True,'process_status':'done'}, context = context)
+        data = self.pool.get('account.voucher').read(cr, uid, context.get('active_ids')[0], context = context)
+        datas = {
+               'ids':context.get('active_ids'),
+               'model':'account.voucher',
+               'form':data,
+               'context':context,
+               }
+        return {
+                'type':'ir.actions.report.xml',
+                'report_name':modele,
+                'datas':datas,
+                'context':context
+                }
 oph_account_voucher_deposit()
-
-# for
-#
