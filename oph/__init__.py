02da29f776f118419075a115420523b2eb4c66e7
Laurent FRANCOIS <francois.oph@gmail.com>
2015-03-29 22:00:42 +1100

    ADD a report in pdf for oph.request. Need to add the script libreoffice.sh for headless libreoffice server

--- oph/__init__.py
+++ oph/__init__.py
@@ -8,6 +8,7 @@ import oph_bloc_agenda
 import oph_reporting
 import oph_filling_template
 import wizard # je pense que le mieux c'est de le mettre à la fin.
+import oph_request
 #import controller
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- oph/oph_request.py
+++ oph/oph_request.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from openerp.osv import fields, osv, orm
 from openerp.tools.translate import _
+import time
+from datetime import datetime
+
 
 
 class oph_request(orm.Model):
     _name = 'oph.request'
+    _inherit = ['mail.thread']
 
     def on_change_oph_partner(self, cr, uid, ids, partner_id, date_request, context = None):
         if context is None:
@@ -61,6 +65,7 @@ class oph_request(orm.Model):
                 'state':fields.selection(_state_get, 'Status', select = True, readonly = True),
                 'company_id': fields.many2one('res.company', 'Company', required = True, change_default = True, readonly = True, states = {'draft':[('readonly', False)]}),
                 'priority_id':fields.selection(_priority_id_selection, 'Priority'),
+                'sent':fields.boolean('Sent'),
                 }
 
     _defaults = {
@@ -68,6 +73,40 @@ class oph_request(orm.Model):
         'company_id': lambda self, cr, uid, c: self.pool.get('res.company')._company_default_get(cr, uid, 'account.invoice', context = c),
         'date_request': lambda *a: time.strftime('%Y-%m-%d'),
                 }
+
+    def action_request_sent(self, cr, uid, ids, context=None):
+        '''
+        This function opens a window to compose an email, with the edi invoice template message loaded by default
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+        ir_model_data = self.pool.get('ir.model.data')
+        try:
+            template_id = ir_model_data.get_object_reference(cr, uid, 'oph', 'email_template_edi_request')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference(cr, uid, 'mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict(context)
+        ctx.update({
+            'default_model': 'oph.request',
+            'default_res_id': ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_request_as_sent': True,
+            })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
 
 class oph_request_line(orm.Model):
     _name = 'oph.request.line'
@@ -89,3 +128,13 @@ class res_partner(orm.Model):
     _columns = {
                 'request_ids': fields.one2many('oph.request.line', 'partner_id', 'Accords CAFAT', readonly = True),
                 }
+    
+class mail_compose_message(osv.Model):
+    _inherit = 'mail.compose.message'
+
+    def send_mail(self, cr, uid, ids, context=None):
+        context = context or {}
+        if context.get('default_model') == 'oph.request' and context.get('default_res_id') and context.get('mark_request_as_sent'):
+            context = dict(context, mail_post_autofollow=True)
+            self.pool.get('oph.request').write(cr, uid, [context['default_res_id']], {'sent': True}, context=context)
+        return super(mail_compose_message, self).send_mail(cr, uid, ids, context=context)
