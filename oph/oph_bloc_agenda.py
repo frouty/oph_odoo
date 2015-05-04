11d45e6513d5aac80f4f8161868f33e0a3efb2f3
Laurent FRANCOIS <francois.oph@gmail.com>
2015-05-04 23:01:32 +1100

    FIX CR hospit odt, FiX operator, add order by name in oph.indication

--- oph/oph_bloc_agenda.py
+++ oph/oph_bloc_agenda.py
@@ -13,11 +13,12 @@ class oph_indication(orm.Model):
     """
     use for OR indications selection
     """
-    _name = "oph.indication"
+    _name = 'oph.indication'
+    _order = 'name asc'
 
     _columns = {
-              'name':fields.char('Name', size = 128),
-              'ivt':fields.boolean('IVT', help = "Tick the box if it's an indication for IVT"),
+              'name':fields.char('Name', size=128),
+              'ivt':fields.boolean('IVT', help="Tick the box if it's an indication for IVT"),
               'comment':fields.text('Comment'),
                  }
     
@@ -28,7 +29,7 @@ class oph_gauge(orm.Model):
     _name = "oph.gauge"
 
     _columns = {
-              'name':fields.char('Name', size = 64),
+              'name':fields.char('Name', size=64),
                  }
 
 class oph_iol_type(orm.Model):
@@ -38,10 +39,10 @@ class oph_iol_type(orm.Model):
     """
     _name = "oph.iol.type"
     _columns = {
-                'name':fields.char("Model", size = 64,),
-                'code':fields.char('Code', size = 16),
-                'manufactor':fields.char('Manufactor', size = 64,),
-                'constant':fields.float('Constant Lens', digits = (16, 2),),
+                'name':fields.char("Model", size=64,),
+                'code':fields.char('Code', size=16),
+                'manufactor':fields.char('Manufactor', size=64,),
+                'constant':fields.float('Constant Lens', digits=(16, 2),),
                 'comment':fields.text('Comment',),
                 'line_ids':fields.one2many('oph.bloc.agenda.line', 'iol_type_id', 'Lines',)
                 }
@@ -53,9 +54,9 @@ class oph_anesthesia_type(orm.Model):
     """
     _name = 'oph.anesthesia.type'
     _columns = {
-                'name':fields.char('Name', size = 64,),
-                'code':fields.char('Code', size = 8,),
-                'comment':fields.char('Comment', size = 128),
+                'name':fields.char('Name', size=64,),
+                'code':fields.char('Code', size=8,),
+                'comment':fields.char('Comment', size=128),
                 'line_ids':fields.one2many('oph.bloc.agenda.line', 'anesthesia_type_id', 'Lines',)
                 }
 
@@ -65,20 +66,20 @@ class oph_procedure_type(orm.Model):
     """
     _name = 'oph.procedure.type'
 
-    def _get_yesorno(self, cursor, user_id, context = None):
+    def _get_yesorno(self, cursor, user_id, context=None):
         return (
                 ('yes', _('YES')),
                 ('no', _('NO')),
                 )
 
     _columns = {
-                'name':fields.char('Name', size = 128, translate = True),
+                'name':fields.char('Name', size=128, translate=True),
                 'shortname':fields.char('Short Name', size=64, translate=True),
-                'code':fields.char('Code', size = 64,),
+                'code':fields.char('Code', size=64,),
                 'dilatation':fields.selection(_get_yesorno, 'Dilatation',),
-                'iol_status':fields.boolean('IOL Status', Help = 'For an IOL during this type of procedure thick the boxe',),
-                'duration':fields.integer('Duration', help = 'Expected duration of procedure in minutes'),
-                'comment':fields.char('Comment', size = 128, help = 'Where you put all the stuff you need for the intervention'),
+                'iol_status':fields.boolean('IOL Status', Help='For an IOL during this type of procedure thick the boxe',),
+                'duration':fields.integer('Duration', help='Expected duration of procedure in minutes'),
+                'comment':fields.char('Comment', size=128, help='Where you put all the stuff you need for the intervention'),
                 'line_ids':fields.one2many('oph.bloc.agenda.line', 'procedure_type_id', 'Lines',)
                 }
 
@@ -88,9 +89,9 @@ class oph_inpatient_type(orm.Model):
     """
     _name = 'oph.inpatient.type'
     _columns = {
-                'name':fields.char('Name', size = 128,),
-                'code':fields.char('Code', size = 64,),
-                'comment':fields.char('Comment', size = 128),
+                'name':fields.char('Name', size=128,),
+                'code':fields.char('Code', size=64,),
+                'comment':fields.char('Comment', size=128),
                 'line_ids':fields.one2many('oph.bloc.agenda.line', 'inpatient_type_id', 'Lines',),
                 }
 
@@ -110,7 +111,7 @@ class oph_bloc_agenda(osv.osv):
     #     return False
     #===========================================================================
 
-    def _get_wdandmonth2(self, cr, uid, ids, field_name, arg, context = {}):
+    def _get_wdandmonth2(self, cr, uid, ids, field_name, arg, context={}):
         """
         will get the week day in text (eg: friday, saturday,...)
         and month 'eg: junuary, february,....)
@@ -128,25 +129,25 @@ class oph_bloc_agenda(osv.osv):
             wd = pytz.UTC.localize(wd)  # make aware datetime object needed for astimezone()
             wd = wd.astimezone(local_tz)  # convert UTC time to local time
             res[record.id] = wd.strftime("%A") + ' ' + wd.strftime("%d") + ' ' + wd.strftime("%B")
-        print "PASSING in : %s. Type of wd is :%s" %(inspect.stack()[0][3], type(res))
+        print "PASSING in : %s. Type of wd is :%s" % (inspect.stack()[0][3], type(res))
         return res
 
-    def _get_wdandmonth(self,cr,uid,ids,field_name,arg,context={}):
+    def _get_wdandmonth(self, cr, uid, ids, field_name, arg, context={}):
         """
         To humanize the date format
         Using the arrow python module.
         """
-        print "CONTEXT:%s" %(context,)
-        res={}
+        print "CONTEXT:%s" % (context,)
+        res = {}
         if context is None:
-            context={}
-        fmt = 'dddd D MMMM YYYY' # to format the date: monday 1 junuary 3021
-        records=self.browse(cr,uid,ids,context)
+            context = {}
+        fmt = 'dddd D MMMM YYYY'  # to format the date: monday 1 junuary 3021
+        records = self.browse(cr, uid, ids, context)
         for record in records:
             if record.name:
                 wd = record.name
-                wd=arrow.get(wd,'YYYY-MM-DD').to('utc').format(fmt,locale=context['lang'])
-                res[record.id]=wd
+                wd = arrow.get(wd, 'YYYY-MM-DD').to('utc').format(fmt, locale=context['lang'])
+                res[record.id] = wd
         return res
     #===========================================================================
     # def name_get(self, cr, uid, ids, context = None):
@@ -161,12 +162,12 @@ class oph_bloc_agenda(osv.osv):
 
     _columns = {
                 'name':fields.date('Date'),
-                'partner_id':fields.many2one('res.partner', 'Owner', help = 'This is the owner of the agenda', domain = [('colleague', '=', True)]),  # We just need colleague here
+                'partner_id':fields.many2one('res.partner', 'Owner', help='This is the owner of the agenda', domain=[('colleague', '=', True)]),  # We just need colleague here
                 'start_date':fields.datetime('StartDate',),
                 'end_date':fields.datetime('EndDate',),
                 'comment':fields.text('Informations',),
-                'wd':fields.function(_get_wdandmonth, method = True, type = 'char', string = 'Weekday',store=False),
-                'active':fields.boolean('Active', help = 'if the active field is set to False, it will allow you to hide the bloc agenda without removing it.'),
+                'wd':fields.function(_get_wdandmonth, method=True, type='char', string='Weekday', store=False),
+                'active':fields.boolean('Active', help='if the active field is set to False, it will allow you to hide the bloc agenda without removing it.'),
                 'line_ids':fields.one2many('oph.bloc.agenda.line', 'bloc_agenda_id', 'Lines',)}
 
     _defaults = {'active': 1, }
@@ -182,45 +183,45 @@ class oph_bloc_agenda_line(osv.osv):
     _name = "oph.bloc.agenda.line"
     _order = "sequence"
 
-    def _get_wdandmonth(self,cr,uid,ids,field_name,arg,context={}):
+    def _get_wdandmonth(self, cr, uid, ids, field_name, arg, context={}):
         """
         To humanize the date format
         Using the arrow python module.
         """
-        res={}
+        res = {}
         if context is None:
-            context={}
-        fmt = 'dddd D MMMM YYYY' # to format the date: monday 1 junuary 3021
-        records=self.browse(cr,uid,ids,context)
+            context = {}
+        fmt = 'dddd D MMMM YYYY'  # to format the date: monday 1 junuary 3021
+        records = self.browse(cr, uid, ids, context)
         for record in records:
             if record.name:
                 wd = record.name
-                wd=arrow.get(wd,'YYYY-MM-DD').to('utc').format(fmt,locale=context['lang'])
-                print 'WD is :%s' %(wd,)
-                res[record.id]=wd
+                wd = arrow.get(wd, 'YYYY-MM-DD').to('utc').format(fmt, locale=context['lang'])
+                print 'WD is :%s' % (wd,)
+                res[record.id] = wd
         return res
 
-    def _ods_get(self, cr, uid, context = None):
+    def _ods_get(self, cr, uid, context=None):
         return [
                 ('OD', _('Right Eye')),
                 ('OS', _('Left Eye')),
                 ]
 
-    def _get_ane_type(self, cursor, user_id, context = None):
+    def _get_ane_type(self, cursor, user_id, context=None):
         return (
                 ('ST', _('Sub-Tenon')),
                 ('PERI', _('Peribulbar')),
                 ('TOP', _('Topique')),
                 )
 
-    def _get_hospit_type(self, cursor, user_id, context = None):
+    def _get_hospit_type(self, cursor, user_id, context=None):
         return (
                 ('AMBU', 'Ambulatoire'),
                 ('1NUIT', '1 Nuit hospit'),
                 ('EXT', 'Externe'),
                 )
 
-    def _get_status_bloc_agenda_line(self, cursor, user_id, context = None):
+    def _get_status_bloc_agenda_line(self, cursor, user_id, context=None):
         return (
                 ('draft', _('Draft')),
                 ('open', _('Open')),
@@ -230,36 +231,36 @@ class oph_bloc_agenda_line(osv.osv):
                 ('no_show', _('No Show')),
                 )
 
-    def statechange_draft(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "draft"}, context = context)
+    def statechange_draft(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "draft"}, context=context)
         return True
 
-    def statechange_open(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "open"}, context = context)
+    def statechange_open(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "open"}, context=context)
         return True
 
-    def statechange_confirm(self, cr, uid, ids, context = None):
-        #from pdb import set_trace;set_trace()
-        self.write(cr, uid, ids, {"state": "confirm"}, context = context)
+    def statechange_confirm(self, cr, uid, ids, context=None):
+        # from pdb import set_trace;set_trace()
+        self.write(cr, uid, ids, {"state": "confirm"}, context=context)
         return True
 
-    def statechange_cancel(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "cancel"}, context = context)
+    def statechange_cancel(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "cancel"}, context=context)
         return True
 
-    def statechange_close(self, cr, uid, ids, context = None):
+    def statechange_close(self, cr, uid, ids, context=None):
         """
         change state to close
         create a record in crm_meeting
         create a record in oph.todolist with tag_id is "Normal", name="CROP"
         create a record in oph.recording ready to print a report
         """
-        self.write(cr, uid, ids, {"state": "close"}, context = context)
+        self.write(cr, uid, ids, {"state": "close"}, context=context)
         print "ids:%s" % ids
         print "context:%s" % context
         if context is None:
             context = {}
-        lines = self.browse(cr, uid, ids, context = context)
+        lines = self.browse(cr, uid, ids, context=context)
 
         # create a meeting for each bloc agenda line
         for l in lines:
@@ -273,7 +274,7 @@ class oph_bloc_agenda_line(osv.osv):
                   'state':'done',
                   'tag':'or',
                   }
-            crm_meeting_obj = self.pool.get('crm.meeting').create(cr, uid, vals_meeting, context = context)
+            crm_meeting_obj = self.pool.get('crm.meeting').create(cr, uid, vals_meeting, context=context)
 
             # create a oph.reporting to be ready to make the OR report
             vals_reporting = {
@@ -285,11 +286,11 @@ class oph_bloc_agenda_line(osv.osv):
                         'iol_power':l.iol_power,
                         'anesthesia_id':l.anesthesia_type_id.id,
                         }
-            reporting_obj = self.pool.get('oph.reporting').create(cr, uid, vals_reporting, context = context)
+            reporting_obj = self.pool.get('oph.reporting').create(cr, uid, vals_reporting, context=context)
         # return True
 
         # Getting a quotation if bloc_agenda_line set to close
-        agenda_line = self.browse(cr, uid, ids[0], context = context)
+        agenda_line = self.browse(cr, uid, ids[0], context=context)
         res = {'default_partner_id':agenda_line.partner_id.id,
                'default_pricelist_id': agenda_line.partner_id.property_product_pricelist.id,
                'default_date_acte':agenda_line.bloc_agenda_id.name,
@@ -305,11 +306,11 @@ class oph_bloc_agenda_line(osv.osv):
             'target': 'current',
         }
 
-    def statechange_noshow(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "no_show"}, context = context)
+    def statechange_noshow(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "no_show"}, context=context)
         return True
 
-    def _get_ic_selection(self, cr, uid, context = None):
+    def _get_ic_selection(self, cr, uid, context=None):
         """
         ic is for informed consent
         """
@@ -319,16 +320,16 @@ class oph_bloc_agenda_line(osv.osv):
                 ('close', 'Close'),
                 )
 
-    def ic_open(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"ic": "open"}, context = context)
+    def ic_open(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"ic": "open"}, context=context)
         return True
 
-    def ic_close(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"ic": "close"}, context = context)
+    def ic_close(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"ic": "close"}, context=context)
         return True
 
-    def onchange_set_duration(self, cr, uid, ids, procedure_type_id, dilatation, context = None):
-        obj_procedure_type = self.pool.get('oph.procedure.type').browse(cr, uid, procedure_type_id, context = None)
+    def onchange_set_duration(self, cr, uid, ids, procedure_type_id, dilatation, context=None):
+        obj_procedure_type = self.pool.get('oph.procedure.type').browse(cr, uid, procedure_type_id, context=None)
         print "ONCHANGE DURATION"
         if obj_procedure_type.dilatation == 'yes':
             dilatation = True
@@ -339,36 +340,36 @@ class oph_bloc_agenda_line(osv.osv):
         return val
 
     _columns = {
-                'name':fields.char('Id', size = 8,),
-                'wd':fields.function(_get_wdandmonth, method = True, type = 'char', string = 'Weekday', store=False),
+                'name':fields.char('Id', size=8,),
+                'wd':fields.function(_get_wdandmonth, method=True, type='char', string='Weekday', store=False),
                 'sequence':fields.integer('Sequence'),
                 'duration':fields.float('Duration',),
-                'start_time':fields.char('Horaire', size = 8),
-                'partner_id':fields.many2one('res.partner', 'Partner', required = True),
-                'state': fields.selection(_get_status_bloc_agenda_line, 'State', readonly = False),
+                'start_time':fields.char('Horaire', size=8),
+                'partner_id':fields.many2one('res.partner', 'Partner', required=True),
+                'state': fields.selection(_get_status_bloc_agenda_line, 'State', readonly=False),
                 'comment':fields.text('Comment'),
-                'ods':fields.selection(_ods_get, 'ODS', required = True,),
+                'ods':fields.selection(_ods_get, 'ODS', required=True,),
                 'snd_eye':fields.boolean('Second Eye'),
                 # 'ane_type':fields.selection(_get_ane_type, 'ANE', required = True,),
                 'anesthesia_type_id':fields.many2one('oph.anesthesia.type', 'Anesthesia Type'),
-                'dilatation':fields.boolean('Dilatation', help = "Tick the box if the patient must be dilated"),
+                'dilatation':fields.boolean('Dilatation', help="Tick the box if the patient must be dilated"),
                 'inpatient_type_id':fields.many2one('oph.inpatient.type', 'Inpatient Type',),
                 'iol_type_id':fields.many2one('oph.iol.type', 'IOL Type'),
-                'iol_power':fields.float('IOL Power', digits = (4, 2)),
+                'iol_power':fields.float('IOL Power', digits=(4, 2)),
                 'bloc_agenda_id':fields.many2one('oph.bloc.agenda', 'Bloc Agenda',),
-                'procedure_type_id':fields.many2one('oph.procedure.type', 'Procedure Type', required = True),
+                'procedure_type_id':fields.many2one('oph.procedure.type', 'Procedure Type', required=True),
                 # 'duration_procedure':fields.related('procedure_type_id', 'duration', type = 'integer', string = 'Duration Procedure'),
-                'gauge_id':fields.many2one('oph.gauge', 'Gauge', help = 'Vitrectomy gauge'),
+                'gauge_id':fields.many2one('oph.gauge', 'Gauge', help='Vitrectomy gauge'),
                 # 'duration_procedure':fields.integer('Duration', 'Estimated procedure duration in minutes'), # Moins bien que le duration fields.float car pas de calcul du total en bas de la colonne
-                'procedure_iol_status':fields.related('procedure_type_id', 'iol_status', type = 'boolean', string = 'IOL Procedure',),
-                'ic':fields.selection(_get_ic_selection, 'Informed Consent', help = 'Check if informed consent has been delivered to and given back by the patient'),
-                'ane_appointment':fields.datetime('Date ANE RDV', help = 'date appointment for anesthesia'),
-                'ane_comment':fields.char('comment ANE', char = '128'),
+                'procedure_iol_status':fields.related('procedure_type_id', 'iol_status', type='boolean', string='IOL Procedure',),
+                'ic':fields.selection(_get_ic_selection, 'Informed Consent', help='Check if informed consent has been delivered to and given back by the patient'),
+                'ane_appointment':fields.datetime('Date ANE RDV', help='date appointment for anesthesia'),
+                'ane_comment':fields.char('comment ANE', char='128'),
                 'po_meeting_id':fields.many2one('crm.meeting', 'PostOR Appointment'),
                 'indication_id':fields.many2one('oph.indication', 'Indication'),
-                #'operator_id':fields.many2one('res.users', 'Operator'),
+                # 'operator_id':fields.many2one('res.users', 'Operator'),
                 'operator_id':fields.many2one('res.partner', 'Operator'),
-                'notification_time':fields.char('Notification Time', size = 8),
+                'notification_time':fields.char('Notification Time', size=8),
                 }
 
     _defaults = {
