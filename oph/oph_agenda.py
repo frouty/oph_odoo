cd4a7fad241b4d878d59eccbab4a0cee876e09c5
Laurent FRANCOIS <francois.oph@gmail.com>
2018-03-04 02:36:44 +1100

    add datewotime in report '

--- oph/oph_agenda.py
+++ oph/oph_agenda.py
@@ -17,7 +17,7 @@ class oph_motive(orm.Model):
     _name = 'oph.motive'
 
     _columns = {
-              'name':fields.char('Name', size = 32,),
+              'name':fields.char('Name', size=32,),
               'comment':fields.text('Comment'),
               }
     _sql_constraints = [
@@ -29,7 +29,7 @@ class crm_meeting(orm.Model):
     _description = "consultations meetings"
     _order = "date asc"
 
-    def get_rt5100(self, cr, uid, ids, context = None):
+    def get_rt5100(self, cr, uid, ids, context=None):
         """Get the datas from the RT-5100
         
         """
@@ -63,11 +63,11 @@ class crm_meeting(orm.Model):
                 
                 # fix axis set to None for axix = 0°
                     # set axis back to 0 when cyl is not None
-                    #OR
+                    # OR
                     _logger.info('In the fix for OR')           
                     if val_measurement.get('cyl_od') is not None and val_measurement.get('axis_od') is None:
                         val_measurement.update({'axis_od':'0'})
-                    #OS
+                    # OS
                     _logger.info('In the fix for OS')
                     if val_measurement.get('cyl_os') is not None and val_measurement.get('axis_os') is None:
                         val_measurement.update({'axis_os':'0'})
@@ -118,30 +118,30 @@ class crm_meeting(orm.Model):
 
 
 
-                oph_measurement_obj = self.pool.get('oph.measurement').create(cr, uid, val_measurement, context = context)
+                oph_measurement_obj = self.pool.get('oph.measurement').create(cr, uid, val_measurement, context=context)
         return True
 
 
 
-    def selection_partner_id(self, cr, uid, ids, context = None):
+    def selection_partner_id(self, cr, uid, ids, context=None):
         """
         Get the partner_id from the res.partner to write it in the crm.meeting record
         """
         res = {}
         fmt = 'YYYY-MM-DD HH:mm:ss'
         print "PASSING IN: %s CONTEXT IS: %s" % (inspect.stack()[0][3], context)
-        self.write(cr, uid, ids, {'partner_id':context.get('default_partner_id'), 'given_date':arrow.now().to('UTC').format(fmt), 'user_id':uid}, context = None)
+        self.write(cr, uid, ids, {'partner_id':context.get('default_partner_id'), 'given_date':arrow.now().to('UTC').format(fmt), 'user_id':uid}, context=None)
         # set state to busy
         self.statechange_busy(cr, uid, ids, context)
         return res
 
-    def default_get(self, cr, uid, fields, context = None):
+    def default_get(self, cr, uid, fields, context=None):
         """ Surcharge la valeur par defaut de la durée d'un RDV"""
-        res = super(crm_meeting, self).default_get(cr, uid, fields, context = context)
+        res = super(crm_meeting, self).default_get(cr, uid, fields, context=context)
         res['duration'] = 0.25
         return res
 
-    def onchange_slot(self, cr, uid, ids, state, date, duration, organizer, context = None):
+    def onchange_slot(self, cr, uid, ids, state, date, duration, organizer, context=None):
         """
         This method to check and avoid creating slot when it's not desirable
         We start by searching the closed slot.
@@ -154,7 +154,7 @@ class crm_meeting(orm.Model):
         print "STATE, DATE, DURATION, ORGANIZER: %s, %s, %s, %s" % (state, date, duration, organizer)
         slot_ids = self.search(cr, uid, [('date', '=', date)])
         print "RESULT OF SEARCH:", slot_ids
-        for record in self.browse(cr, uid, slot_ids, context = context):
+        for record in self.browse(cr, uid, slot_ids, context=context):
             print "RECORD DATE IS;", record.date
             print "DATE_DEADLINE IS", record.date_deadline
             print "PARTNER NAME:", record.partner_id.name
@@ -166,19 +166,19 @@ class crm_meeting(orm.Model):
            return {'value': res.get('value', {}), 'warning':warning}
         return {'value': {}}
 
-    def onchange_partner_id(self, cr, uid, ids, state, given_date, context = None):
+    def onchange_partner_id(self, cr, uid, ids, state, given_date, context=None):
         """
         Set state to busy when partner_id not empty
         Set given_date is date when the appointement is given
         Set user_id to the user who give the appointment
         """
         print "PASSING IN: %s CONTEXT IS: %s" % (inspect.stack()[0][3], context)
-        foo = self.read(cr, uid, ids, fields = ['write_date', ], context = context, load = '_classic_read')
+        foo = self.read(cr, uid, ids, fields=['write_date', ], context=context, load='_classic_read')
         fmt = 'YYYY-MM-DD HH:mm:ss'
         return {'value':{'state':'busy', 'given_date':arrow.now().to('UTC').format(fmt), 'user_id':uid}}
 
-    def onchange_dates(self, cr, uid, ids, start_date, duration = False, end_date = False, allday = False, context = None):
-       res = super(crm_meeting, self).onchange_dates(cr, uid, ids, start_date, duration = duration, end_date = end_date, allday = allday, context = context)
+    def onchange_dates(self, cr, uid, ids, start_date, duration=False, end_date=False, allday=False, context=None):
+       res = super(crm_meeting, self).onchange_dates(cr, uid, ids, start_date, duration=duration, end_date=end_date, allday=allday, context=context)
        slot_ids = self.search(cr, uid, [('date', '=', start_date)])
        if slot_ids:
            res.update({'warning': {
@@ -207,23 +207,23 @@ class crm_meeting(orm.Model):
     #             )
     #===========================================================================
 
-    def statechange_draft(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "draft"}, context = context)
+    def statechange_draft(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "draft"}, context=context)
         return True
-    def statechange_open(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "open"}, context = context)
+    def statechange_open(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "open"}, context=context)
         return True
-    def statechange_close(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "close"}, context = context)
+    def statechange_close(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "close"}, context=context)
         return True
-    def statechange_cancel(self, cr, uid, ids, context = None):
+    def statechange_cancel(self, cr, uid, ids, context=None):
         """
         Set the state of crm.meeting record to cancel
         and create a new crm.meeting lot
         with the cancel crm.meeting
         """
-        self.write(cr, uid, ids, {"state": "cancel"}, context = context)
-        vals = self.read(cr, uid, ids, fields = ['date', 'duration', 'date_deadline', 'tag' ], context = context, load = '_classic_read')
+        self.write(cr, uid, ids, {"state": "cancel"}, context=context)
+        vals = self.read(cr, uid, ids, fields=['date', 'duration', 'date_deadline', 'tag' ], context=context, load='_classic_read')
         # vals est une liste de dictionnaire avec les données des records
        # from pdb import set_trace;set_trace()
         for record in vals:  # on boucle sur les données des record retournées.
@@ -234,23 +234,23 @@ class crm_meeting(orm.Model):
             del record['id']
             # del context['default_partner_id']
             record.update({'name':'Ouvert', 'state':record['tag'], 'partner_id':False})
-            self.create(cr, uid, record, context = context)
+            self.create(cr, uid, record, context=context)
         return True
 
-    def statechange_in(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "in"}, context = context)
+    def statechange_in(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "in"}, context=context)
         return True
-    def statechange_in_between(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "in_between"}, context = context)
+    def statechange_in_between(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "in_between"}, context=context)
         return True
-    def statechange_out(self, cr, uid, ids, context = None):
+    def statechange_out(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
         # set meeting to close
-        self.write(cr, uid, ids, {"state": "done"}, context = context)
+        self.write(cr, uid, ids, {"state": "done"}, context=context)
         # return True #uncomment if just want the change state to out
         # get info for the quotation
-        meeting = self.browse(cr, uid, ids[0], context = context)  # comment if you don't want to open a quotation view
+        meeting = self.browse(cr, uid, ids[0], context=context)  # comment if you don't want to open a quotation view
        # pricelist = part.property_product_pricelist and part.property_product_pricelist.id or False
         res = {'default_partner_id': meeting.partner_id.id,
                'default_pricelist_id': meeting.partner_id.property_product_pricelist.id,
@@ -270,29 +270,30 @@ class crm_meeting(orm.Model):
             'target': 'current',
         }
 
-    def statechange_free(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "done", "free":True}, context = context)
+    def statechange_free(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "done", "free":True}, context=context)
         return True
 
-    def statechange_no_show(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "no_show"}, context = context)
+    def statechange_no_show(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "no_show"}, context=context)
         return True
 
-    def statechange_busy(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "busy"}, context = context)
+    def statechange_busy(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "busy"}, context=context)
         return True
 
-    def statechange_wait(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "wait"}, context = context)
+    def statechange_wait(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "wait"}, context=context)
         return True
 
-    def statechange_nwnm(self, cr, uid, ids, context = None):
-        self.write(cr, uid, ids, {"state": "nwnm"}, context = context)
+    def statechange_nwnm(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {"state": "nwnm"}, context=context)
         return True
 
-    def _get_datewotime(self, cr, uid, ids, field_name, arg, context = {}):
+    def _get_datewotime(self, cr, uid, ids, field_name, arg, context={}):
         """
         will get the date without timestamp from date
+        and in local time.
         giving possibility to search by date 
         """
         res = {}
@@ -309,7 +310,7 @@ class crm_meeting(orm.Model):
             # print "In _GET_DATEWOTIME. res is: %s" % res
         return res
 
-    def _format_fullmotive(self, cr, uid, ids, name, args, context = None):
+    def _format_fullmotive(self, cr, uid, ids, name, args, context=None):
         """
         Concatenate the motive and motive comment 
         to get the fullmotive
@@ -317,7 +318,7 @@ class crm_meeting(orm.Model):
         and get real information for patient motive
         """
         res = {}
-        for br in self.browse(cr, uid, ids, context = None):
+        for br in self.browse(cr, uid, ids, context=None):
             motive = br.motive.name or ''
             motivecomment = br.motive_comment or ''
             fullmotive = motive + ' ' + motivecomment
@@ -325,10 +326,10 @@ class crm_meeting(orm.Model):
         return res
 
     _columns = {
-                'subject':fields.char('Subject', size = 128, help = "Object of the meeting",),  # not sure it's usefull
+                'subject':fields.char('Subject', size=128, help="Object of the meeting",),  # not sure it's usefull
                 'motive':fields.many2one('oph.motive', 'Motive',),
-                'motive_comment':fields.char('Comment', size = 128, help = 'Comment to precise the motive'),
-                'fullmotive':fields.function(_format_fullmotive, type = 'char', size = 128, string = 'Full Motive', store = True, method = True),
+                'motive_comment':fields.char('Comment', size=128, help='Comment to precise the motive'),
+                'fullmotive':fields.function(_format_fullmotive, type='char', size=128, string='Full Motive', store=True, method=True),
                 'chief_complaint':fields.text('Chief Complaint'),
                 'state': fields.selection([
                                                     ('draft', _('Draft')),
@@ -344,35 +345,35 @@ class crm_meeting(orm.Model):
                                                     ('in', _('In')),
                                                     ('in_between', _('In Between')),
                                                     ('done', 'Out'),
-                                                    ('office', _('Office'))], readonly = False),
+                                                    ('office', _('Office'))], readonly=False),
                 'partner_id':fields.many2one('res.partner', 'Partner',),
-                'tono_ids':fields.one2many('oph.measurement', 'meeting_id', 'Tonometry', domain = [('type_id.code', '=', 'tono')]),
-                'refraction_ids':fields.one2many('oph.measurement', 'meeting_id', 'Refraction', domain = [('type_id.code', '=', 'ref')]),
-                'keratometry_ids':fields.one2many('oph.measurement', 'meeting_id', 'Keratometry', domain = [('type_id.code', '=', 'ker')]),
-                'va_ids':fields.one2many('oph.measurement', 'meeting_id', 'Visual Acuity', domain = [('type_id.code', '=', 'va')]),
-                'sle_ids':fields.one2many('oph.measurement', 'meeting_id', 'Slit Lamp Exam', domain = [('type_id.code', '=', 'sle')]),
-                'pachy_ids':fields.one2many('oph.measurement', 'meeting_id', 'Center Corneal Thickness', domain = [('type_id.code', '=', 'pachy')]),
-                'datewotime':fields.function(_get_datewotime, method = True, type = 'date', string = 'DateWOtime', store = True),
+                'tono_ids':fields.one2many('oph.measurement', 'meeting_id', 'Tonometry', domain=[('type_id.code', '=', 'tono')]),
+                'refraction_ids':fields.one2many('oph.measurement', 'meeting_id', 'Refraction', domain=[('type_id.code', '=', 'ref')]),
+                'keratometry_ids':fields.one2many('oph.measurement', 'meeting_id', 'Keratometry', domain=[('type_id.code', '=', 'ker')]),
+                'va_ids':fields.one2many('oph.measurement', 'meeting_id', 'Visual Acuity', domain=[('type_id.code', '=', 'va')]),
+                'sle_ids':fields.one2many('oph.measurement', 'meeting_id', 'Slit Lamp Exam', domain=[('type_id.code', '=', 'sle')]),
+                'pachy_ids':fields.one2many('oph.measurement', 'meeting_id', 'Center Corneal Thickness', domain=[('type_id.code', '=', 'pachy')]),
+                'datewotime':fields.function(_get_datewotime, method=True, type='date', string='DateWOtime', store=True),
                 'todo_list_ids':fields.one2many('oph.todolist', 'meeting_id', 'TODO',),
                 #===============================================================
                 'medication_line_ids':fields.one2many('oph.medication.line', 'meeting_id', 'Medication Line'),
                 # cut/paste to inherit class crm.meeting cf oph_prescription.py
                 #===============================================================
                 'reporting_line_ids':fields.one2many('oph.reporting', 'meeting_id', 'Reporting Line'),
-                'conclusion_ids':fields.one2many('oph.measurement', 'meeting_id', 'Conclusion Line', domain = [('type_id.code', '=', 'conc')]),
-                'miscellaneous_ids':fields.one2many('oph.measurement', 'meeting_id', 'Miscellaneous informations', domain = [('type_id.code', '=', 'misc')]),
+                'conclusion_ids':fields.one2many('oph.measurement', 'meeting_id', 'Conclusion Line', domain=[('type_id.code', '=', 'conc')]),
+                'miscellaneous_ids':fields.one2many('oph.measurement', 'meeting_id', 'Miscellaneous informations', domain=[('type_id.code', '=', 'misc')]),
                 'tag':fields.selection([
                                         ('office', _('Office')),
                                         ('or', _('OR')),
                                         ('cs', _('Consultation')),  # Add for persistence usefull for change to cancel
                                         ('tech', _('Technique')),  # Add for persistence usefull for change to cancel
-                                        ], 'Tag', select = True, readonly = True),
-                'free':fields.boolean('Free', help = 'True if not invoiced'),  # for free consultation
+                                        ], 'Tag', select=True, readonly=True),
+                'free':fields.boolean('Free', help='True if not invoiced'),  # for free consultation
                 'neuro':fields.text('Neuro Observation'),
                 'mh':fields.text('Medical History'),
                 # 'allergia':fields.one2many('oph.allergen', 'meeting_id', 'Allergia'),
-                'pricelist':fields.related('partner_id', 'property_product_pricelist', type = 'many2one', relation = 'product.pricelist', string = 'Pricelist', store = False),
-                'given_date':fields.datetime('Given Date', help = 'Date when the appointement is given to the partner'),
+                'pricelist':fields.related('partner_id', 'property_product_pricelist', type='many2one', relation='product.pricelist', string='Pricelist', store=False),
+                'given_date':fields.datetime('Given Date', help='Date when the appointement is given to the partner'),
                 }
 
     _defaults = {
@@ -382,17 +383,17 @@ class crm_meeting(orm.Model):
                  'tag':'cs',
                  'free':False }
 
-    def unlink(self, cr, uid, ids, context = None):
-        for meeting in self.browse(cr, uid, ids, context = context):
+    def unlink(self, cr, uid, ids, context=None):
+        for meeting in self.browse(cr, uid, ids, context=context):
             if meeting.state not in ('draft', 'open'):
                 raise osv.except_osv(_('Error!'), _('Impossible to delete a meeting not in draft state  or open!'))
-        return super(crm_meeting, self).unlink(cr, uid, ids, context = context)
+        return super(crm_meeting, self).unlink(cr, uid, ids, context=context)
 
 class oph_measurement(orm.Model):
     _inherit = "oph.measurement"
     _columns = {
-                'chief_complaint':fields.related('meeting_id', 'chief_complaint', type = 'char', string = 'Chief Complaint',),
-                'motive':fields.related('meeting_id', 'motive', type = 'many2one', relation = 'oph.motive', store = True, string = "Motive"),
+                'chief_complaint':fields.related('meeting_id', 'chief_complaint', type='char', string='Chief Complaint',),
+                'motive':fields.related('meeting_id', 'motive', type='many2one', relation='oph.motive', store=True, string="Motive"),
                 }
 # '
              #
