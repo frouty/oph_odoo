d34056997d0f969b168167b5b0edfcbc1b84419e
Laurent FRANCOIS <francois.oph@gmail.com>
2018-06-16 02:22:55 +1100

    fiv

--- oph/custom/oph_account_invoice.py
+++ oph/custom/oph_account_invoice.py
@@ -30,30 +30,30 @@ class account_invoice(orm.Model):
             ('BdC', 'Baie des Citrons'),
             ('CAFAT Mut', 'CAFAT Mutuelle'),
             ('CSSR', 'CSSR') ], 'Payeur'),  # required=True),
-        'ref_statement': fields.char('Statement Ref', size = 32, help = 'Reference of the statement for bank reconcilation'),
+        'ref_statement': fields.char('Statement Ref', size=32, help='Reference of the statement for bank reconcilation'),
         'date_acte':fields.date('Appointment Date'),
-        'payment_method':fields.char('Payment Method', size = 16, help = 'Payment method to help tracking paiement'),
+        'payment_method':fields.char('Payment Method', size=16, help='Payment method to help tracking paiement'),
         }
 
-    def invoice_pay_customer(self, cr, uid, ids, context = None):
+    def invoice_pay_customer(self, cr, uid, ids, context=None):
         """Set check owner to partner_id by default"""
-        res = super(account_invoice, self).invoice_pay_customer(cr, uid, ids, context = context)
+        res = super(account_invoice, self).invoice_pay_customer(cr, uid, ids, context=context)
         print "IN CUSTOM INVOICE_PAY_CUSTOMER"
         partner_obj = self.pool.get('res.partner')
-        br = partner_obj.browse(cr, uid, res['context']['default_partner_id'], context = None)
+        br = partner_obj.browse(cr, uid, res['context']['default_partner_id'], context=None)
         res['context'].update({'default_check_owner' : br.name})
         return res
 
-    def invoice_cash(self, cr, uid, ids, context = None):
+    def invoice_cash(self, cr, uid, ids, context=None):
         # from pdb import set_trace; set_trace()
         print "IN INVOICE VALIDATE CASH"
-        self.write(cr, uid, ids, {'payment_method':'cash', }, context = context)
+        self.write(cr, uid, ids, {'payeur':'Cash', }, context=context)
         return True
 
-    def invoice_check(self, cr, uid, ids, context = None):
+    def invoice_check(self, cr, uid, ids, context=None):
         # from pdb import set_trace; set_trace()
         print "IN INVOICE VALIDATE CHECK"
-        self.write(cr, uid, ids, {'payment_method':'check'}, context = context)
+        self.write(cr, uid, ids, {'payment_method':'check'}, context=context)
         return True
 
 
