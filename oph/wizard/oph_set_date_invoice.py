a7b104ab7bb1ec74c4084c69f318fcc84cd125ed
Laurent FRANCOIS <francois.oph@gmail.com>
2019-03-30 16:17:29 +1100

    add pdb again

--- oph/wizard/oph_set_date_invoice.py
+++ oph/wizard/oph_set_date_invoice.py
@@ -38,10 +38,10 @@ class oph_set_date_invoice(osv.osv_memory):
               context = {}
         pool_obj = pooler.get_pool(cr.dbname)
         data_inv = pool_obj.get('account.invoice').read(cr,uid,context['active_ids'], ['date_acte'],context=context)
-        import pdb
-        pdb.set_trace()
+        
         for record in data_inv:
-            print record
+            import pdb
+            pdb.set_trace()
         return {'type': 'ir.actions.act_window_close'}
         
         
