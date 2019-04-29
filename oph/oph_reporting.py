ed127f50348d5717d148997025442edfadfe0a29
laurent FRANCOIS <francois.oph@gmail.com>
2019-04-30 10:13:35 +1100

    add capsular tension ring

--- oph/oph_reporting.py
+++ oph/oph_reporting.py
@@ -23,13 +23,13 @@ class oph_lens(orm.Model):
     _name = "oph.lens"
 
     _columns = {
-              'name':fields.char('Name', size=64),
-              'model':fields.char('Model', size=64),
+              'name':fields.char('Name', size = 64),
+              'model':fields.char('Model', size = 64),
               'magnification':fields.float('Magnification'),
                  }
-    
 
-# # je vais plutot utiliser la class product.product 
+
+# # je vais plutot utiliser la class product.product
 # # au lieu de creer un autre objet
 # class oph_scleral_buckle_product(orm.Model):
 #     """
@@ -48,7 +48,7 @@ class oph_lens(orm.Model):
 class oph_reporting(orm.Model):
     _name = 'oph.reporting'
 
-    def _type_get(self, cr, uid, context=None):
+    def _type_get(self, cr, uid, context = None):
         return [
                 ('Off', _('Office Report')),
                 ('Laser', _('Laser Report')),
@@ -59,14 +59,14 @@ class oph_reporting(orm.Model):
                 ('IVT', _('IntraVitreal Injection')),
                  ]
 
-    def create(self, cr, uid, data, context=None):
+    def create(self, cr, uid, data, context = None):
         if data['receiver_partner'] is True:
             receiver = self.pool.get('crm.meeting').browse(cr, uid, data['meeting_id']).partner_id.id
             data['receiver_id'] = receiver
-        result = super(oph_reporting, self).create(cr, uid, data, context=context)
+        result = super(oph_reporting, self).create(cr, uid, data, context = context)
         return result
 
-    def on_change_partner(self, cr, uid, ids, receiver_partner, context=None):
+    def on_change_partner(self, cr, uid, ids, receiver_partner, context = None):
         """
         Get the id of the patient if tick the box the receiver is the patient
         and send it to "receiver_id" field
@@ -82,35 +82,35 @@ class oph_reporting(orm.Model):
             values['receiver_id'] = receiver
         return {'value':values}
 
-    def on_change_receiver(self, cr, uid, ids, receiver_id, context=None):
+    def on_change_receiver(self, cr, uid, ids, receiver_id, context = None):
         if context is None:
             context = {}
         values = {}
         obj = self.pool.get('oph.honorific')
-        res = obj.search(cr, uid, [('user_id', '=', uid), ('partner_id', '=', receiver_id)], context=context)
+        res = obj.search(cr, uid, [('user_id', '=', uid), ('partner_id', '=', receiver_id)], context = context)
         if not res:  # si search ne trouve rien alors il retourne une liste vide []
             values = {'honorific':True}
             return {'value':values}
-        res = obj.read(cr, uid, res, fields=['honorific'], context=context)
+        res = obj.read(cr, uid, res, fields = ['honorific'], context = context)
         values = {'honorific':res[0].get('honorific')}  # provisoire pour les tests
         return {'value':values}
 
-    def _state_get(self, cr, uid, context=None):
+    def _state_get(self, cr, uid, context = None):
         return [
                     ('draft', 'Draft'),
                     ('open', 'Open'),
                     ('close', 'Close'),
                     ]
 
-    def statechange_open(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {"state": "open"}, context=context)
+    def statechange_open(self, cr, uid, ids, context = None):
+        self.write(cr, uid, ids, {"state": "open"}, context = context)
         return True
 
-    def statechange_close(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {"state": "close"}, context=context)
+    def statechange_close(self, cr, uid, ids, context = None):
+        self.write(cr, uid, ids, {"state": "close"}, context = context)
         return True
 
-    def __ods_get(self, cr, uid, context=None):
+    def __ods_get(self, cr, uid, context = None):
         return (
                 ('OD', _('Right Eye')),
                 ('OS', _('Left Eye')),
@@ -121,7 +121,7 @@ class oph_reporting(orm.Model):
 #         set_trace()
 #===============================================================================
 
-    def _get_default_name(self, cr, uid, context=None):
+    def _get_default_name(self, cr, uid, context = None):
         """
         Return a string 
         "Report-Patient Name-date of the day"
@@ -130,12 +130,12 @@ class oph_reporting(orm.Model):
         if context == None:
             context = {}
         obj_meeting = self.pool.get('crm.meeting')
-        name = obj_meeting.browse(cr, uid, context['active_id'], context=context).partner_id.fullname
+        name = obj_meeting.browse(cr, uid, context['active_id'], context = context).partner_id.fullname
         # import pdb;pdb.set_trace()
         date = arrow.now().to(context['tz']).format("DD-MM-YYYY")
         return "Report_" + name + '_' + date
 
-    def _get_next_meeting(self, cr, uid, context=None):
+    def _get_next_meeting(self, cr, uid, context = None):
         """
         Return a string
         Return month is in french not very convinient for internationalisation..
@@ -146,28 +146,28 @@ class oph_reporting(orm.Model):
 
         ny = arrow.now()
         if ny.day >= 15:
-            ny = ny.replace(months=+13)
+            ny = ny.replace(months = +13)
         else:
-            ny = ny.replace(months=+12)
+            ny = ny.replace(months = +12)
         ny = mappedmonth[ny.month] + ' ' + str(ny.year)
         print "PASSING IN:%s. CONTEXT:%s" % (inspect.stack()[0][3], context)
         print "RETURN: %s" % (ny,)
         return ny
 
-    def get_default_body_text(self, cr, uid, id, context=None):
+    def get_default_body_text(self, cr, uid, id, context = None):
         """
         Will get the body text from the template_id record
         """
         if context is None:
             context = {}
         template_obj = self.pool.get('oph.reporting.template')
-        for report in self.browse(cr, uid, id, context=context):
-            template_rec = template_obj.read(cr, uid, report.template_id.id, ['text_body', ], context=context)
-            self.write(cr, uid, report.id, {'text_body':template_rec.get('text_body', '')}, context=context)
+        for report in self.browse(cr, uid, id, context = context):
+            template_rec = template_obj.read(cr, uid, report.template_id.id, ['text_body', ], context = context)
+            self.write(cr, uid, report.id, {'text_body':template_rec.get('text_body', '')}, context = context)
         return True
 
 # # I will use the datewotime field instead.
-# # I keep for further references.    
+# # I keep for further references.
 #     def _format_appointment(self, cr, uid, ids, field_name, args, context=None):
 #         res = {}
 #        # import pdb;pdb.set_trace()
@@ -179,34 +179,34 @@ class oph_reporting(orm.Model):
 #             else:
 #                 res[rec.id] = None
 #         return res
-    
+
     _columns = {
-              'name':fields.char('Name', size=128, help="Name report"),
+              'name':fields.char('Name', size = 128, help = "Name report"),
               'type':fields.selection(_type_get, 'Type'),
-              'meeting_id':fields.many2one('crm.meeting', 'Meeting', select=True),
-              'partner_id':fields.related("meeting_id", "partner_id", type="many2one", relation="res.partner", string="Partner", store=True, readonly=True,),
-             
+              'meeting_id':fields.many2one('crm.meeting', 'Meeting', select = True),
+              'partner_id':fields.related("meeting_id", "partner_id", type = "many2one", relation = "res.partner", string = "Partner", store = True, readonly = True,),
+
              # # many problems with date
-              'date':fields.related('meeting_id', 'date', type='date', string='Consultation Date', store=True),
+              'date':fields.related('meeting_id', 'date', type = 'date', string = 'Consultation Date', store = True),
               # field 'date' above is pretty bad. Doesn't  convert correctly to local time.
               # and it lost the utc time so you can do nothing to get the local time.
               # 'appointment':fields.related('meeting_id', 'date', type='datetime', string='Consultation Date', store=True),
               # but i don't need a timestamp just the day date
               # 'local_day':fields.function(_format_appointment, type='char', string='local day', store=True, method=True),
-              'datewotime':fields.related('meeting_id', 'datewotime', type='date', string='Consultation Date', store=True),
-              
-              'header_id':fields.many2one('oph.reporting.template', string='Header', domain=[('type', '=', 'H')], help="Header for OR report"),
-              'template_id':fields.many2one('oph.reporting.template', 'Reporting Template', select=True),
-              'text_body':fields.text('Text Body', help='Text body of the report'),
+              'datewotime':fields.related('meeting_id', 'datewotime', type = 'date', string = 'Consultation Date', store = True),
+
+              'header_id':fields.many2one('oph.reporting.template', string = 'Header', domain = [('type', '=', 'H')], help = "Header for OR report"),
+              'template_id':fields.many2one('oph.reporting.template', 'Reporting Template', select = True),
+              'text_body':fields.text('Text Body', help = 'Text body of the report'),
               # many2one ou many2many?
               # Il n'y qu'un seul template par record template donc c'est pas un many2many donc c'est un one2one==many2one
               # 'template_ids':fields.many2many('oph.reporting.template', 'oph_reporting_reporting_template_rel', 'reporting_id', 'reporting_template_id', 'Reporting Templates', domain = [('active', '=', True)]),
               # coordonnées du destinataire pour remplir l'entete automatiquement
-              'receiver_id':fields.many2one('res.partner', 'Receiver', domain=[('colleague', '=', True)]),
-              'receiver_partner':fields.boolean('Receiver Patient', help="Patient is the receiver"),
-              'cc_partner':fields.boolean('Copie Patient', help="Thick the box, if sending a copie to the patient"),
-              'dictate_wpatient':fields.boolean('Dictate with patient', help="Thick the box, if dictate with patient"),
-              'cc_ids':fields.many2many('res.partner', 'reporting_partner_rel', 'reporting_id', 'partner_id', 'Copies', domain=[('colleague', '=', True)]),
+              'receiver_id':fields.many2one('res.partner', 'Receiver', domain = [('colleague', '=', True)]),
+              'receiver_partner':fields.boolean('Receiver Patient', help = "Patient is the receiver"),
+              'cc_partner':fields.boolean('Copie Patient', help = "Thick the box, if sending a copie to the patient"),
+              'dictate_wpatient':fields.boolean('Dictate with patient', help = "Thick the box, if dictate with patient"),
+              'cc_ids':fields.many2many('res.partner', 'reporting_partner_rel', 'reporting_id', 'partner_id', 'Copies', domain = [('colleague', '=', True)]),
               # a qui j'écris au patient lui meme dans ce cas là c'est direct par partner_id
               #
               # faire un champ "adressé par" dans le crm_meeting à remplir par la secrétaire.
@@ -219,16 +219,16 @@ class oph_reporting(orm.Model):
               'impact_size':fields.integer('Impact Size'),
               'impact_number':fields.integer('Impacts number'),
               'HbA1c':fields.float('HbA1c'),
-              'next_meeting':fields.char('Next Year', size=32),
-              'quand':fields.char('When', size=16),
-             
+              'next_meeting':fields.char('Next Year', size = 32),
+              'quand':fields.char('When', size = 16),
+
               # 'molecule':fields.selection(_molecule_get, 'Molecule', help = "Intravitreal Injected molecule(s)"),
               # I want to have more than one molecule
               'molecule_ids':fields.many2many('oph.inn', 'oph_reporting_inn_rel', 'reporting_id', 'inn_id', 'Molecule(s)'),  # marche pas domain = [('ivt', 'is', True)],
               'indication_id':fields.many2one('oph.indication', 'Indication'),
               'ods':fields.selection([('OD', _('Right Eye')),
                                       ('OS', _('Left Eye')),
-                                      ('ODS', _('Right and Left Eye')), ], 'ODS', required=False,),
+                                      ('ODS', _('Right and Left Eye')), ], 'ODS', required = False,),
               'state':fields.selection(_state_get, 'State'),
               # text_body:fields.text qui serait construit par concaténation des text_body des templates et insertions des données var1....
               'honorific':fields.boolean('Honorific'),
@@ -251,34 +251,34 @@ class oph_reporting(orm.Model):
               'ccoct_or':fields.text('CC OR'),
               'ccoct_os':fields.text('CC OS'),
               # for OR
-              'iol_power':fields.float('IOL power', digits=(4, 2)),
-              'iol_type':fields.char('IOL Type', size=16),
-              'var1':fields.char('US Total Time', size=16),
-              'var2':fields.float('PT', help="Puissance Totale equivalente en position 3"),
-              'var3':fields.float('EDC', help="Energie dissipée cumulée"),
-              'var4':fields.char('TT', size=16, help="Temps de Torsion"),
-              'var5':fields.float('Amp T M', help="Amplitude de Torsion Moyenne"),
-              'var6':fields.float('Amp T M P3', help="Amplitude de Torsion Moyenne en position 3"),
-              'var7':fields.float('Amp T M Eq P3', help="Amplitude de Torsion Moyenne equivalente en position 3"),
-              'bv':fields.boolean('Blue Vision', help='Use of Blue Vision'),
+              'iol_power':fields.float('IOL power', digits = (4, 2)),
+              'iol_type':fields.char('IOL Type', size = 16),
+              'var1':fields.char('US Total Time', size = 16),
+              'var2':fields.float('PT', help = "Puissance Totale equivalente en position 3"),
+              'var3':fields.float('EDC', help = "Energie dissipée cumulée"),
+              'var4':fields.char('TT', size = 16, help = "Temps de Torsion"),
+              'var5':fields.float('Amp T M', help = "Amplitude de Torsion Moyenne"),
+              'var6':fields.float('Amp T M P3', help = "Amplitude de Torsion Moyenne en position 3"),
+              'var7':fields.float('Amp T M Eq P3', help = "Amplitude de Torsion Moyenne equivalente en position 3"),
+              'bv':fields.boolean('Blue Vision', help = 'Use of Blue Vision'),
               'sutureless':fields.boolean('Sutureless'),
               'subtenon':fields.boolean('Subtenon'),
               'air_bubble':fields.boolean('Air Bubble'),
-              'gauge_id':fields.many2one('oph.gauge', 'Gauge', help='Vitrectomie gauge'),
-              'comment':fields.text('Comment', help="Useful for some reminder"),
+              'gauge_id':fields.many2one('oph.gauge', 'Gauge', help = 'Vitrectomie gauge'),
+              'comment':fields.text('Comment', help = "Useful for some reminder"),
               'anesthesia_id':fields.many2one('oph.anesthesia.type', 'Anesthesia'),
               'post_op_treatement':fields.text('Post Operatoire Treatement'),
               'positionnement':fields.text('Positionnement'),
               'operating_machine_id': fields.many2one('product.product', 'Operating machine'),
               'vitreotome_machine':fields.text('Vitreotome mention'),
-              'tech_op_phako':fields.text('Tech op phako', help='Tech op phako information'),
+              'tech_op_phako':fields.text('Tech op phako', help = 'Tech op phako information'),
              # #
               'lens_status':fields.selection([('phake', _('Phakic')),
                                                         ('pseudophake', _('Pseudophakic')),
                                                         ('aphake', _('Aphakic')),
                                                         ], 'Status Lens'),
              # 'indentation':fields.selection([('Eponge siliconée, 2X5'), _('')),]),
-             'scleral_buckel_product_id':fields.many2one('product.product', 'Scleral buckle Product', domain=[('categ_id.name', '=', 'Scleral Buckles Products')]),
+             'scleral_buckel_product_id':fields.many2one('product.product', 'Scleral buckle Product', domain = [('categ_id.name', '=', 'Scleral Buckles Products')]),
              'macula_onoff':fields.selection([('on', _('On')),
                                                         ('off', _('Off'))],
                                                          'macula ON/OFF'),
@@ -287,9 +287,10 @@ class oph_reporting(orm.Model):
 #                                             ('c3f8', _('C3F8')),
 #                                             ('hs', _('silicone oil'))],
 #                                               'Tamponnade'),
-            'tamponnade':fields.many2one('product.product', 'Tamponnade Product', domain=[('categ_id.name', '=', 'Ophthalmic Surgical Liquids and Gas')]),
-            'irisdevice':fields.boolean('irisdevice', help='use of an iris device'),
-              
+            'tamponnade':fields.many2one('product.product', 'Tamponnade Product', domain = [('categ_id.name', '=', 'Ophthalmic Surgical Liquids and Gas')]),
+            'irisdevice':fields.boolean('irisdevice', help = 'use of an iris device'),
+            'capsulartensionring':fields.boolean('irisdevice', help = 'use of an capsular tension ring'),
+
                             }
 
     _defaults = {
@@ -306,7 +307,7 @@ class oph_reporting_template(orm.Model):
     _name = 'oph.reporting.template'
     _order = 'name asc'
 
-    def _type_get(self, cr, uid, context=None):
+    def _type_get(self, cr, uid, context = None):
         return [('Off', _('Office Report')),
                 ('ORR', _('Operating Room Report')),
                 ('DIAB', _('Diabetic')),
@@ -323,9 +324,9 @@ class oph_reporting_template(orm.Model):
                 ]
 
     _columns = {
-              'name':fields.char('Name', size=128, help="Name of the template"),
-              'short_name':fields.char('Short Name', size=64),
-              'text_body':fields.text('Text Body', help='Text body of the template'),
+              'name':fields.char('Name', size = 128, help = "Name of the template"),
+              'short_name':fields.char('Short Name', size = 64),
+              'text_body':fields.text('Text Body', help = 'Text body of the template'),
               'type':fields.selection([('Off', _('Office Report')),
                                                 ('ORR', _('Operating Room Report')),
                                                 ('DIAB', _('Diabetic')),
@@ -340,8 +341,8 @@ class oph_reporting_template(orm.Model):
                                                 ('FAF', _('Fluoresceine Angiography Report')),
                                                 ('OCT', _('OCT Report')),
                                                 ], 'Type'),
-              'active':fields.boolean('Active', help="If False the template wont be selectable"),
-              'product_ids':fields.many2many('product.product', 'oph_technique_procedure_rel', 'reporting_template_id', 'product_id', 'Technique', domain=[('sale_ok', '=', False)]),
+              'active':fields.boolean('Active', help = "If False the template wont be selectable"),
+              'product_ids':fields.many2many('product.product', 'oph_technique_procedure_rel', 'reporting_template_id', 'product_id', 'Technique', domain = [('sale_ok', '=', False)]),
               }
 
     _defaults = {
