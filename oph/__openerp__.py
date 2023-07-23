814f72faeecc63bfbb4ab2938a2d983451898d67
Laurent FRANCOIS <francois.oph@gmail.com>
2023-07-23 22:01:36 +1100

    remove editable="bottom" in view tree bloc agenda line

--- oph/__openerp__.py
+++ oph/__openerp__.py
@@ -67,6 +67,7 @@ Permet de faire pleins de choses absolument géniales
                 'wizard/oph_account_voucher_deposit_view.xml',
                 'wizard/oph_crm_meeting_state_view.xml',
                 'wizard/oph_day_template_view.xml',
+                'wizard/oph_bloc_agenda_line_payd.xwl',
                 # 'wizard/oph_day_template_agenda_factory_view.xml',
                 'oph_prescription_view.xml',
                 'data/surgery_data.xml',
