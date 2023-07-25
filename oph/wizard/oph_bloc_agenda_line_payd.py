3705a5c087bc56f277cbc1d5771f6677204c153c
Laurent FRANCOIS <francois.oph@gmail.com>
2023-07-25 18:34:58 +1100

    set def bloc_line_paid using set to draft for crm meeting

--- oph/wizard/oph_bloc_agenda_line_payd.py
+++ oph/wizard/oph_bloc_agenda_line_payd.py
@@ -7,38 +7,21 @@ from openerp import pooler
 
 class oph_bloc_agenda_line_paid(osv.osv_memory):
     """
-    This wizard will set the payed field to True if 
-    paid
+    This wizard will set the payed field to True the all selected bloc_agenda_line 
     """
 
     _name = "oph.bloc.agenda.line.paid"
-    _description = "Set the paid to True"
+    _description = "Set the paid to True the selected bloc agenda line"
     
     def bloc_line_paid(self, cr, uid, ids, context = None):
         print "Je passe par bloc_line_paid"
         if context is None:
             context = {}
-        from pdb import set_trace; set_trace()
-        modele = 'bdxcheck'
+        #from pdb import set_trace; set_trace()
         pool_obj = pooler.get_pool(cr.dbname)
-        #data_inv = pool_obj.get(context.get('active_model')).read(cr, uid, context['active_ids'], ['journal_id','process_status'], context = context)
-        data_inv = pool_obj.get(context.get('active_model')).read(cr, uid, context['active_ids'],context = context)
-        import pdb; pdb.set_trace()
+        data_inv = pool_obj.get(context.get('active_model')).read(cr, uid, context['active_ids'],['paid'],context = context)
         for record in data_inv:
-            pool_obj.get(context.get('active_model')).write(cr, uid, record['id'], {'Paid':True,}, context = context)
-        data = self.pool.get('oph.bloc.agenda.line').read(cr, uid, context.get('active_ids')[0], context = context)
-        datas = {
-               'ids':context.get('active_ids'),
-               'model':'oph.bloc.agenda.line',
-               'form':data,
-               'context':context,
-               }
-        return {
-                #'type':'ir.actions.report.xml',
-                #'report_name':modele,
-                'datas':datas,
-                'context':context
-                }
-
+            pool_obj.get(context.get('active_model')).write(cr, uid, record['id'], {'paid':True,}, context = context)
+        return {'type': 'ir.actions.act_window_close'}
 
 oph_bloc_agenda_line_paid()
